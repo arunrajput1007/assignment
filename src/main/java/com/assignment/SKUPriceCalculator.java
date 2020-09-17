70083e88d3f70b366e370dac02b8b1f2b034f169
arunrajput1007 <arunrajput1007@gmail.com>
2020-09-17 22:06:21 +0530

    calculate price method implemented

--- src/main/java/com/assignment/SKUPriceCalculator.java
+++ src/main/java/com/assignment/SKUPriceCalculator.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -17,7 +18,16 @@ public class SKUPriceCalculator {
 
     public Double calculatePrice(Expression expression){
         if(expression instanceof SKUnit){
-
+            //find in scheme
+            //else find in unit price
+            final SKUnit skExpr = (SKUnit)expression;
+            Optional<Map.Entry<Expression,Double>> schemePrice = scheme.entrySet().stream()
+                    .filter(entry -> entry.getKey() instanceof SKUnit)
+                    .filter(entry -> ((SKUnit) entry.getKey()).getSku() == skExpr.getSku() &&
+                            skExpr.getUnit() >= ((SKUnit) entry.getKey()).getUnit()).findFirst();
+            return schemePrice.map(entry -> entry.getValue() +
+                    calculatePrice(new SKUnit(skExpr.getUnit() - ((SKUnit) entry.getKey()).getUnit(),((SKUnit) entry.getKey()).getSku())))
+                    .orElseGet(() -> unitPrice.get(skExpr.getSku()) * skExpr.getUnit());
         }else if(expression instanceof And){
             And andExpr = (And)expression;
             return calculatePrice(andExpr.getExp1()) + calculatePrice(andExpr.getExp2());
